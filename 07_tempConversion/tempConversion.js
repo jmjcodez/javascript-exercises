69c1db26160327a65ecf13b6f8fc8ee112767c74
jmjcodez <johnmjones94@gmail.com>
2024-12-28 18:49:54 -0500

    Modify function to simplify code Uses Arrow declaration and round instead of toFixed to avoid having to convert to string - simplified math

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,7 @@
-const convertToCelsius = function(tempFah) {
-  return Number(((tempFah - 32) * (5/9)).toFixed(1));
-};
+const convertToCelsius = tempFah => Math.round((tempFah - 32) * (5 / 9) * 10) / 10;
 
-const convertToFahrenheit = function(tempCel) {
-  return Number((tempCel * (9/5 + 32)).toFixed(1));
-};
-
+const convertToFahrenheit = tempCel => Math.round(((tempCel * 9) / 5 + 32) * 10) / 10;
+  
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
