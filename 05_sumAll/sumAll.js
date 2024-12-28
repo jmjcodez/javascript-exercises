376baf4ab37fe32246b5a88961ee8bc16846071d
jmjcodez <johnmjones94@gmail.com>
2024-12-28 16:56:56 -0500

    Create function to sum numbers between inputs
    Uses for loop to sum between range, passes first test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum,lastNum) {
+	let numSum = 0;
+	for (i = firstNum-1; i < lastNum; i++ ){
+		numSum += (i+1);
+	}
+	return numSum;
 };
 
 // Do not edit below this line
