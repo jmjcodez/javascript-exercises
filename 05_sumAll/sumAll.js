83761a4cc0fc7a641a0224bab2bcc206220f8c78
jmjcodez <johnmjones94@gmail.com>
2024-12-28 17:14:29 -0500

    Modify function to ensures correct arguments Allows numbers to be submitted in any order, and ensures integer, and positive numbers only, excluding any Nan type

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,22 @@
 const sumAll = function(firstNum,lastNum) {
+	
+	if(firstNum > 0 && 
+		 lastNum > 0 && 
+		 typeof firstNum === 'number' &&
+		 typeof lastNum === 'number' && 
+		 firstNum % 1 === 0 && 
+		 lastNum % 1 ===0 ){
+	maxNum = Math.max(firstNum,lastNum);
+	minNum = Math.min(firstNum,lastNum);
 	let numSum = 0;
-	for (i = firstNum-1; i < lastNum; i++ ){
+	for (i = minNum-1; i < maxNum; i++ ){
 		numSum += (i+1);
 	}
 	return numSum;
-};
-
+}
+else{
+	return "ERROR";
+}
+}
 // Do not edit below this line
 module.exports = sumAll;
