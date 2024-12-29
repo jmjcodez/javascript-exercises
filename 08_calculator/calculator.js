ae2cb86c1c86e1f906ac780042d390b629850e46
jmjcodez <johnmjones94@gmail.com>
2024-12-29 00:55:28 -0500

    Add function to calculate factorial Uses for loop to calculate factorial of any number input. Completes the assignment and passes all tests.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -26,8 +26,12 @@ const power = function(a, b) {
   return a ** b;
 };
 
-const factorial = function(a) {
-	
+const factorial = function(a){
+  let result = 1;
+  for (i = a; i > 0; i--){
+    result *= i;
+  }
+	return result;
 };
 
 // Do not edit below this line
