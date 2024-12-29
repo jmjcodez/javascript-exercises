fe6613e86ae4d3d36a3fbf214821f09c3cac6431
jmjcodez <johnmjones94@gmail.com>
2024-12-28 19:09:11 -0500

    Create functions for partial completion of calculator assignment Completed and tested all functions, less the factorial function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  for (const num of array){
+    sum += num;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = 1;
+  for (const num of array){
+    result *= num;
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
+const factorial = function(a) {
 	
 };
 
