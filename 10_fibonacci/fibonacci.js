e3851010b7d727869601dbe84d64f7d42e0da919
jmjcodez <johnmjones94@gmail.com>
2024-12-29 01:53:07 -0500

    Create function to return specified value in Fibonacci sequence Used an iterative array created with a for loop and push function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+	isNumber = Number(a);
+	if (isNumber < 0){
+		return "OOPS"
+	}
+	else if (isNumber === 0){
+		return isNumber;
+	}
+	else{
+		let array = [1,1];
+		for (i=2; i < isNumber; i++){
+		array.push(array[i-1] + array[i-2]);
+	}
+	return array[isNumber-1];
+	}
 };
 
 // Do not edit below this line
